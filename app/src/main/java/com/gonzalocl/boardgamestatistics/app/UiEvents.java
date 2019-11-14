a386d19335dea66155bcad902b7536bedfb32afc
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-14 01:41:56 +0100

    uievents

---
+++ app/src/main/java/com/gonzalocl/boardgamestatistics/app/UiEvents.java
@@ -0,0 +1,56 @@
+package com.gonzalocl.boardgamestatistics.app;
+
+public class UiEvents {
+
+
+    private static final UiEvents uiEvents = new UiEvents();
+
+    private UiEvents() {
+
+    }
+
+    public static UiEvents getUiEvents() {
+        return uiEvents;
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void addPlayer(int i) {
+
+    }
+
+    public void deletePlayer(int i) {
+
+    }
+
+    public void clearPlayerList() {
+
+    }
+
+    public void setGameName(int i) {
+
+    }
+
+    public void setLocation(int i) {
+
+    }
+
+    public String[] getSuggestedPlayers() {
+        return null;
+    }
+
+    public String[] getSuggestedGameNames() {
+        return null;
+    }
+
+    public String[] getSuggestedLocations() {
+        return null;
+    }
+
+}
--- app/src/main/java/com/gonzalocl/boardgamestatistics/ui/MainActivity.java
+++ app/src/main/java/com/gonzalocl/boardgamestatistics/ui/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.gonzalocl.boardgamestatistics.R;
 import com.gonzalocl.boardgamestatistics.app.PlayerList;
+import com.gonzalocl.boardgamestatistics.app.UiEvents;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final UiEvents uiEvents = UiEvents.getUiEvents();
+
+        Button buttonStart = findViewById(R.id.button_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uiEvents.deletePlayer(0);
+            }
+        });
+
+
+        Button addPlayer = findViewById(R.id.add_player);
+        addPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uiEvents.addPlayer(0);
+            }
+        });
+
+        Button clearPlayerList = findViewById(R.id.clear_player_list);
+        clearPlayerList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uiEvents.clearPlayerList();
+            }
+        });
 
         RecyclerView playerList = findViewById(R.id.player_list);
         playerList.setHasFixedSize(true);
