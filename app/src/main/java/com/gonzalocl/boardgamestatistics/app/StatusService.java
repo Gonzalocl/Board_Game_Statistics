e2894dfcdb13de1911106e17d0516338a131d393
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-23 04:07:38 +0100

    status service

--- app/src/main/java/com/gonzalocl/boardgamestatistics/app/StatusService.java
+++ app/src/main/java/com/gonzalocl/boardgamestatistics/app/StatusService.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 
 import androidx.annotation.Nullable;
 
+import com.gonzalocl.boardgamestatistics.ui.StatusNotification;
+
 public class StatusService extends IntentService {
 
 
@@ -19,6 +21,22 @@ public class StatusService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+
+        StatusNotification statusNotification = StatusNotification.getStatusNotification(this);
+
+        startForeground(statusNotification.getNotificationId(), statusNotification.getNotification());
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            statusNotification.updateNotification(String.format("[%d]", i));
+
+        }
+
 
     }
 }
